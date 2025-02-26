a5067df0d9072d78a6f91a2e0081b09b2e417df1
FahimX <sakibfahim270@gmail.com>
2025-02-26 12:51:34 +0800

    adding toggle hnadler

---
+++ utilities/toggle.js
@@ -0,0 +1,18 @@
+document.getElementById("cash-out").style.display = "none";
+document
+  .getElementById("add-money-box")
+  .addEventListener("click", function (event) {
+    handleToggle("add-money", "block");
+    handleToggle("cash-out", "none");
+  });
+
+document
+  .getElementById("cash-out-box")
+  .addEventListener("click", function (event) {
+    handleToggle("add-money", "none");
+    handleToggle("cash-out", "block");
+  });
+
+function handleToggle(id, status) {
+  document.getElementById(id).style.display = status;
+}
